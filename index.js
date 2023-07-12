974bea3a706642ad36402ef02325197118893d1a
Ryan Lester <hacker@linux.com>
2023-07-11 22:30:17 -0400

    migrate from deprecated ipfs-core to helia

--- index.js
+++ index.js
@@ -1,29 +1,14 @@
-const {MemoryDatastore} = require('interface-datastore');
-const IPFS = require('ipfs-core');
-const {
-	createRepo,
-	locks: {memory: memoryLock}
-} = require('ipfs-repo');
-const rawCodec = require('multiformats/codecs/raw');
+const HeliaUnixFS = import('@helia/unixfs');
+const Helia = import('helia');
+const Multiformats = import('multiformats/cid');
 
-const ipfs = IPFS.create({
-	repo: createRepo(
-		'/dev/null',
-		() => rawCodec,
-		{
-			blocks: new MemoryDatastore(),
-			datastore: new MemoryDatastore(),
-			keys: new MemoryDatastore(),
-			pins: new MemoryDatastore(),
-			root: new MemoryDatastore()
-		},
-		{autoMigrate: false, repoLock: memoryLock, repoOwner: true}
-	)
-});
+const ipfs = Helia.then(async ({createHelia}) =>
+	(await HeliaUnixFS).unixfs(await createHelia())
+);
 
 const ipfsFetch = async (hash, {timeout} = {}) => {
 	const result = (async () => {
-		const iter = (await ipfs).cat(hash);
+		const iter = (await ipfs).cat((await Multiformats).CID.parse(hash));
 		const buffers = [];
 		for await (const buf of iter) {
 			buffers.push(buf);
